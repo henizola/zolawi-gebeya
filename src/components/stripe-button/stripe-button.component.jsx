736989f9c6f4711711cc849cafd043294b6b949c
henizola <z.w.henok@gmail.com>
2020-04-29 20:03:51 +0300

    integrated stripe payment , updated favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+
+
+
+const StripeCheckoutButton=({price})=>{
+    const priceForStripe=price*100;
+    const  publishableKey='pk_test_gwT7oJSK2Wey1H7fCVOJ44LD00K5pjIkrA';
+
+const onToken=token=>{
+    console.log(token);
+    alert("Payment Succesfull")
+}
+
+    return (
+        <StripeCheckout
+        lable='Pay Now'
+        name="Zolawi Gebeya"
+        billingAddress
+        shipingAddress
+        description={`your total is $ ${price}`}
+        amount={priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 import {
   selectCartItems,
   selectCartTotal
@@ -34,6 +35,13 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className="test-warning">
+      *please user the following test payment craditcard for payments
+      <br/>
+      4242 4242 4242 4242  -expire 1/21  CVV 123
+
+    </div>
+    <StripeCheckoutButton price={total}/>
   </div>
 );
 
